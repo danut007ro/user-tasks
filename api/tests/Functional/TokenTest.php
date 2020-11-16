98d8ef095eca832dee070380fffd45f1cb9de42a
Daniel Gorgan <danut007ro@gmail.com>
2020-11-16 23:44:52 +0200

    Add authentication tests

---
+++ api/tests/Functional/TokenTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+
+class TokenTest extends ApiTestCase
+{
+    use RefreshDatabaseTrait;
+
+    public function testAuthenticate(): void
+    {
+        static::createClient()->request('POST', '/authentication_token', [
+            'json' => [
+                'email' => 'user1@docler.com',
+                'password' => 'docler1',
+            ],
+        ]);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertMatchesJsonSchema([
+            'type' => 'object',
+            'properties' => [
+                'token' => [
+                    'type' => 'string',
+                ],
+            ],
+            'required' => ['token'],
+        ]);
+    }
+}
